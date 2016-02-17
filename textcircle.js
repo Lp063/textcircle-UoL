7c1558f2dbcfdd82d925f83c2a2eaaf66ce32ef2
Lp063 <lohit.pereira09@stanislausbandra.in>
2016-02-17 12:16:45 +0530

    installed new package

---
+++ textcircle.js
@@ -0,0 +1,24 @@
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient) {
+	Template.editor.helpers({
+		docid:function(){
+			var doc=Documents.findOne();
+			if(doc){
+				return doc._idl
+			}else{
+				return undefined;
+			}
+		}
+	})
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if(!Documents.findOne()){
+    	//No Docs yet
+    	Documents.insert({title:"My new Document"});
+    }
+  });
+}
