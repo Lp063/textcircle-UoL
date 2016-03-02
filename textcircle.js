5867f70bb65840a582fac45fa8043de55fff47ed
Lp063 <lohit.pereira09@stanislausbandra.in>
2016-03-02 10:32:13 +0530

    cannot list active users for document

--- textcircle.js
+++ textcircle.js
@@ -15,6 +15,8 @@ if (Meteor.isClient) {
 
 		config:function(){
 			return function(editor){
+				editor.setOption("lineNumbers",true);
+				editor.setOption("theme","cobalt");
 				editor.on("change",function(cm_editor,info){
 				console.log(cm_editor.getValue());
 				$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
@@ -23,6 +25,27 @@ if (Meteor.isClient) {
 			}
 		}
 	});
+
+	Template.editingUsers.helpers({
+		users:function(){
+			var doc,eusers,users;
+			doc=Documents.findOne();
+			if(!doc){return;} //givr up
+			eusers=EditingUsers.FindOne({docid:doc._id});
+			if(!eusers){return;} // give up
+			users = new Array();
+			var i=0;
+			for(var user_id in eusers.users)
+			{
+				console.log("Adding a user");
+				console.log(eusers.users[user_id]);
+				users[i]=fixObjectKeys(eusers.users[user_id]);
+				i++;
+			}
+			return users;
+		}
+	});
+
 }
 
 
@@ -36,19 +59,37 @@ if (Meteor.isServer) {
   });
 }
 
+
 Meteor.methods({
 	addEditingUser:function(){
 		var doc, user, eusers;
 
-		doc=Documents.findOne();
-		if(!doc){return;}
-		if(!this.userId){return;}
+		doc = Documents.findOne();
+		if(!doc){return;} //No Doc Give up.
+		if(!this.userId){return;}// No Loggen in user Give up.
+		//NOw i have a doc anf possibly a user.
 
 		user=Meteor.user().profile;
 		eusers=EditingUsers.findOne({docid:doc._id});
 		if(!eusers){
-			eusers={docid=doc._id,};
+			eusers={
+				docid:doc._id,
+				users:{},
+			};
 		}
-		EditingUsers.insert({user:"Matthew"});
+		user.lastEdit = new Date();
+		eusers.users[this.userId] = user;
+		EditingUsers.upsert({_id:eusers._id},eusers);
 	}
-});
+}); 
+
+// this renames object keys by removing hyphens to make the compatible 
+// with spacebars. 
+function fixObjectKeys(obj){
+  var newObj = {};
+  for (key in obj){
+    var key2 = key.replace("-", "");
+    newObj[key2] = obj[key];
+  }
+  return newObj;
+}
